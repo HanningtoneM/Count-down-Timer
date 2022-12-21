de398ccbfb6959da39069a1667ee6c4bbc1f4c6b
HanningtoneM <hanningtonepompyy@gmail.com>
2022-12-21 09:46:42 +0300

    Changed the time and date

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ window.addEventListener('load', () => {
     let totalSeconds;
     
     function init() {
-        totalSeconds = Math.floor((new Date('11/04/2022') - new Date()) / 1000); 
+        totalSeconds = Math.floor((new Date('05/25/2023') - new Date()) / 1000); 
         setTimeLeft();
         let interval = setInterval(() => {
             if (totalSeconds < 0) {
